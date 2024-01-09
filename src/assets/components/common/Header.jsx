20a5be1afe4db2e5ef8c5270221216f2e27a6f9b
sukyung <sukyung6999@gmail.com>
2024-01-10 00:01:53 +0900

    add: 공통 내비, 레이아웃

---
+++ src/assets/components/common/Header.jsx
@@ -0,0 +1,11 @@
+import Nav from './Nav';
+
+import './Header.css'
+
+const Header = () => {
+  return <header className="header">
+    <h1 className="logo_head"><a href="">MovieIT</a></h1>
+    <Nav/>
+  </header>
+}
+export default Header;
---
+++ src/assets/pages/Main.jsx
@@ -0,0 +1,36 @@
+import classes from './Main.module.css'
+
+const Main = () => {
+  return <div>
+    <div className={classes.section_main}>
+      <div className={classes.bg}></div>
+      <div className="cont_area">
+        <ul>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+        </ul>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+      </div>
+    
+    </div>
+    <div className="section_other">
+      <div className="cont_area">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis fugit perspiciatis culpa voluptatum! Maiores tenetur est natus alias, reprehenderit ex sint vero, non fugit optio iusto magnam libero enim molestiae.</p>
+      </div>
+    </div>
+  </div>
+}
+export default Main;
