ce082abd50aab240b12ac44631759830aa5dffdc
이수찬 <lovetmfqud@naver.com>
2024-01-12 18:13:25 +0900

    add: 테라박스 퍼블페이지 push

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,66 @@
 import { useState } from 'react';
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Box, Container, Typography, Tabs, Tab, AppBar, Toolbar, Card, CardContent, CardMedia } from '@mui/material';
 
-function App() {
-  const [count, setCount] = useState(0);
+const theme = createTheme();
+
+const App = () => {
+  const [tabIndex, setTabIndex] = useState('0');
+
+  const handleTabChange = (event, newValue) => {
+    setTabIndex(newValue);
+  };
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <ThemeProvider theme={theme}>
+      <Box display="flex" flexDirection="column" alignItems="center" minHeight="100vh">
+        {/* Header */}
+        <AppBar position="static" color="secondary">
+          <Toolbar>
+            <Tabs value={tabIndex} onChange={handleTabChange} indicatorColor="primary" textColor="inherit" style={{ marginRight: 'auto' }}>
+              <Tab label="영화" value="0" />
+              <Tab label="예매" value="1" />
+            </Tabs>
+            <Typography variant="h6" style={{ flex: 1, textAlign: 'center' }}>
+              TeraBox
+            </Typography>
+            <Tabs value={tabIndex} onChange={handleTabChange} indicatorColor="primary" textColor="inherit" style={{ marginLeft: 'auto' }}>
+              <Tab label="극장" value="2" />
+              <Tab label="이벤트" value="3" />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <Box display="flex" gap={7} style={{ marginTop: '50px' }}>
+            <Card style={{ width: '600px' }}>
+              <CardMedia component="img" height="500" image="src\image\1.jpg" style={{ objectFit: 'cover' }} />
+              <CardContent>
+                <Typography variant="h6" style={{ textAlign: 'center', marginTop: '10px' }}>
+                  외계+인 2부
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card style={{ width: '600px' }}>
+              <CardMedia component="img" height="500" image="src\image\2.jpg" style={{ objectFit: 'cover' }} />
+              <CardContent>
+                <Typography variant="h6" style={{ textAlign: 'center', marginTop: '10px' }}>
+                  위시
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card style={{ width: '600px' }}>
+              <CardMedia component="img" height="500" image="src\image\3.jpg" style={{ objectFit: 'cover' }} />
+              <CardContent>
+                <Typography variant="h6" style={{ textAlign: 'center', marginTop: '10px' }}>
+                  서울의 봄
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
