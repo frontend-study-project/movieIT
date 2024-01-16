094a14a15b96aeaa6bf26866db1d1bcd50341af6
sukyung <sukyung6999@gmail.com>
2024-01-16 14:28:27 +0900

    modify: 머지전 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import "./App.css";
-import Header from "./assets/components/common/Header";
-import Main from "./assets/pages/Main";
 
 function App() {
   return (
-    <div className="dark">
-      <Header />
-      <Main/>
+    <div>
     </div>
   );
 }
