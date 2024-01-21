35b40458a5d1baf43caa19d30d3047fda27500ac
suld2495 <suld7777@gmail.com>
2024-01-21 23:41:54 +0900

    feat: 마이페이지 레이아웃 css 수정

--- src/layouts/mypage/MypageLayout.jsx
+++ src/layouts/mypage/MypageLayout.jsx
@@ -3,13 +3,16 @@ import layout from '../layout.module.css';
 import Header from '../Header';
 import { Outlet } from 'react-router-dom';
 import MypageNav from './MypageNav';
+import { Box } from '@mui/material';
 
 const MypageLayout = () => (
   <>
     <Header />
     <main className={`${layout.main} ${styled.main}`}>
       <MypageNav />
-      <Outlet />
+      <Box flex={1}>
+        <Outlet />
+      </Box>
     </main>
   </>
 )
