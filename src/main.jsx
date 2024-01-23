8b04b009b8ae9ec79f334392071710914d7c115a
suld2495 <suld7777@gmail.com>
2024-01-23 22:24:26 +0900

    refactor: SnackbarProvider 랩핑한 컴포넌트로 리팩토링

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { SnackbarProvider } from 'material-ui-snackbar-provider'
 
 import './styles/reset.css'
 import './index.css'
+import MSnackbarProvider from './components/provider/MSnackbarProvider.jsx'
 
 const enableMocking = async () => {
   // eslint-disable-next-line no-undef
@@ -21,9 +21,9 @@ enableMocking().then(() => {
   
   ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
-      <SnackbarProvider SnackbarProps={{ autoHideDuration: 4000 }}>
+      <MSnackbarProvider>
         <RouterProvider router={router} />
-      </SnackbarProvider>
+      </MSnackbarProvider>
     </QueryClientProvider>,
   );
 });
