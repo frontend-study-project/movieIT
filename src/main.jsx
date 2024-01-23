3fc17f032166b29c4ae26626d8851234d7ae4883
mskim <wemb@wembui-Macmini.local>
2024-01-23 16:23:51 +0900

    feat: Provider 추가
    react-query, 스낵바에 대한 provider를 추가하였습니다.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SnackbarProvider } from 'material-ui-snackbar-provider'
 
 import './styles/reset.css'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />,
-)
+const enableMocking = async () => {
+  // eslint-disable-next-line no-undef
+  if (process.env.NODE_ENV !== 'development') return;
+
+  const { worker } = await import('./mocks/browser');
+
+  return worker.start();
+};
+
+enableMocking().then(() => {
+  const queryClient = new QueryClient();
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <QueryClientProvider client={queryClient}>
+      <SnackbarProvider SnackbarProps={{ autoHideDuration: 4000 }}>
+        <RouterProvider router={router} />
+      </SnackbarProvider>
+    </QueryClientProvider>,
+  );
+});
