e758b567a60e741b09d31bdb70dc9040d3b911a2
suld2495 <suld7777@gmail.com>
2024-01-27 11:27:01 +0900

    feat: 헤더 링크 추가 및 배경컬러 변경

---
+++ src/layouts/AuthNav.jsx
@@ -0,0 +1,28 @@
+import { Box } from "@mui/system";
+import { useFetchUserQuery } from "../hooks/useAuth";
+import { Link } from "react-router-dom";
+import styled from './auth.nav.module.css';
+
+const AuthNav = ({ isSub }) => {
+  const { data: user } = useFetchUserQuery();
+  return (
+    <Box className={`${styled.authnav} ${isSub ? styled.sub : ''}`}>
+      <Box component="ul">
+        {
+          user ? (
+            <>
+              <li><Link to="/login">로그인</Link></li>
+              <li><Link to="/join">회원가입</Link></li>
+            </>
+          ) : (
+            <>
+              <li><Link to="#logout">로그아웃</Link></li>
+            </>
+          )
+        }
+      </Box>
+    </Box>
+  )
+};
+
+export default AuthNav;
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,10 +1,21 @@
+import { useMemo } from "react";
 import Nav from "./Nav";
 import styled from './header.module.css';
+import { useLocation } from 'react-router-dom';
+import AuthNav from "./AuthNav";
+
+const isSub = (pathname) => pathname !== '/';
 
 const Header = () => {
+  const { pathname } = useLocation();
+  const className = useMemo(() => (
+    `${styled.header} ${styled.main} ${!isSub(pathname) ? styled.header_background : ''}`
+  ), [pathname]);
+
   return (
-    <header className={`${styled.header} ${styled.main}`}>
-      <Nav />
+    <header className={className}>
+      <AuthNav />
+      <Nav isSub={isSub(pathname)} />
     </header>
   )
 };
