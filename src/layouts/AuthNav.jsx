51aafbb3093b5d4dbf728e6544b5d90939ea4065
suld2495 <suld7777@gmail.com>
2024-01-27 21:20:32 +0900

    feat: 로그인 기능 추가

--- src/layouts/AuthNav.jsx
+++ src/layouts/AuthNav.jsx
@@ -1,17 +1,19 @@
 import { Box } from "@mui/system";
-import { useFetchUserQuery } from "../hooks/useAuth";
+import { useFetchUserQuery, useLogoutMutation } from "../hooks/useAuth";
 import { Link } from "react-router-dom";
 import styled from './auth.nav.module.css';
 
 const AuthNav = ({ isSub }) => {
   const { data: user } = useFetchUserQuery();
+  const logout = useLogoutMutation();
+  
   return (
     <Box className={`${styled.authnav} ${isSub ? styled.sub : ''}`}>
       <Box component="ul">
         {
           user ? (
             <>
-              <li><Link to="#logout">로그아웃</Link></li>
+              <li><Link to="#logout" onClick={logout.mutate}>로그아웃</Link></li>
               <li><Link to="/book">빠른예매</Link></li>
             </>
           ) : (
