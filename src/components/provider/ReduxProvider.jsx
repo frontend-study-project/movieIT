5632d5d8d274d73921115e0e08d522eec3e031b9
suld2495 <suld7777@gmail.com>
2024-01-30 21:56:43 +0900

    fix: children 설정

--- src/components/provider/ReduxProvider.jsx
+++ src/components/provider/ReduxProvider.jsx
@@ -1,8 +1,8 @@
 import { Provider } from 'react-redux';
 import store from '../../store';
 
-const ReduxProvider = () => {
-  return <Provider store={store} />
+const ReduxProvider = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>
 };
 
 export default ReduxProvider;
