9d334a7df7b06f8bdaec5bdbdddca6ba858d02b6
yh.cho <youngho7482@gmail.com>
2024-02-14 16:01:58 +0900

    feat:페이지 타이틀 공통 컴포넌트

---
+++ src/components/common/Title/Title.jsx
@@ -0,0 +1,6 @@
+import styled from "./title.module.css";
+
+const Title = ({ title }) => {
+  return <h2 className={styled.main_title}>{title}</h2>;
+};
+export default Title;
--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -1,5 +1,10 @@
+import Title from "../../components/common/Title/Title";
 import styled from "./movie.module.css";
 const Movie = () => {
-  return <div>영화</div>;
+  return (
+    <div className={styled.wrapper_movie}>
+      <Title title="영화" />
+    </div>
+  );
 };
 export default Movie;
