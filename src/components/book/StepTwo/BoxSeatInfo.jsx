b969538febcd494f312a96566ba0e8ae9adae87e
sukyung <sukyung6999@gmail.com>
2024-03-07 23:03:59 +0900

    update: 서버로 전달하는 날짜와 시간 형식 수정

--- src/components/book/StepTwo/BoxSeatInfo.jsx
+++ src/components/book/StepTwo/BoxSeatInfo.jsx
@@ -86,7 +86,7 @@ const BoxSeatInfo = () => {
         "auditorium" : '',
         "people" : totalNum,
         "seat" : selectedSeats,
-        "date": date.slice(0,9) + runningTime.timeStart,
+        "date": date.slice(0,10) + ' '  + runningTime.timeStart,
         "money": totalPrice
     })
     })
