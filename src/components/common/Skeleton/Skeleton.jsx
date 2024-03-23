8a4d0709bd85f7864e493599c1b92fc8192a3d15
sukyung <sukyung6999@gmail.com>
2024-03-23 13:49:59 +0900

    modify: 스켈레톤 수정, 선호영화 아이콘 제거

--- src/components/common/Skeleton/Skeleton.jsx
+++ src/components/common/Skeleton/Skeleton.jsx
@@ -1,10 +1,10 @@
 import Skeleton from '@mui/material/Skeleton';
-import Stack from '@mui/material/Stack';
+import Stack from '@mui/material/Box';
 
-export default function SkeletonBox({width, height, color}) {
+export default function SkeletonBox({width, height, color, style}) {
   return (
     <Stack spacing={1}>
-      <Skeleton animation="wave" sx={{ bgcolor: `grey.${color}` }} variant="rounded" width={width} height={height} />
+      <Skeleton animation="wave" sx={{ bgcolor: `grey.${color}` }} variant="rounded" width={width} height={height} style={style} />
     </Stack>
   );
 }
